8e539cbbc876a09a1a8e43288ecbcdb9082e3aa2
1g2g <wonji126326@gmail.com>
2023-08-21 16:42:53 +0900

    feat : emotion 세팅 및 글로벌 스타일 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Global } from '@emotion/react';
 import './App.css';
+import { globalStyles } from './assets/common';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Global styles={globalStyles} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/assets/common.js
@@ -0,0 +1,20 @@
+import { css } from '@emotion/react';
+
+export const globalStyles = css`
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  button {
+    cursor: pointer;
+    outline: none;
+  }
+`;
+
+export const palette = {
+  themeColor: '#ffd26b',
+  backgroundColor: '#222222',
+};
