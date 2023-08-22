c805799860ba2cc06f8351bc37c0254eda8f31f5
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-08-22 17:12:28 +0900

    [✨Fix]/movies삭제

--- src/routes/movieRouter.js
+++ src/routes/movieRouter.js
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Error from '../components/Error';
 import Root from '../components/Root';
-import Movies from '../components/Movies';
 
 export const movieRouter = createBrowserRouter([
   {
@@ -9,10 +8,6 @@ export const movieRouter = createBrowserRouter([
     element: <Root />,
     errorElement: <Error />,
     children: [
-      {
-        path: '/movies',
-        element: <Movies />,
-      },
       {
         path: '/movies/:movieID',
         element: <h3></h3>,
