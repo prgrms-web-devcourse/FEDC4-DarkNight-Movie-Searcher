5e30b91bda09549abf834f9f7b92dc706735d473
kutta97 <yhjin04170@gmail.com>
2023-08-22 17:32:39 +0900

    feat: SearchBar 컴포넌트에 useForm hook 연결

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropType from 'prop-types';
 import styled from '@emotion/styled';
 import TextInput from '../Input/TextInput';
+import useForm from '../../hooks/useForm';
 
 const SearchBarForm = styled.form`
   display: flex;
@@ -10,19 +11,33 @@ const SearchBarForm = styled.form`
   color: #ffd26b;
   border-radius: 24px;
   background-color: #222;
+
   &:focus-within {
     border: 1px solid #333;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
   }
 `;
 
 const SearchBar = ({ onSubmit }) => {
+  const { errors, isLoading, handleChange, handleSubmit } = useForm({
+    initialValues: { title: '' },
+    onSubmit,
+    validate: ({ title }) => {
+      const newErrors = {};
+      if (!title) newErrors.title = 'Please enter the movie title';
+      return newErrors;
+    },
+  });
+
   return (
-    <SearchBarForm onSubmit={onSubmit}>
+    <SearchBarForm onSubmit={handleSubmit}>
       <TextInput
         name="search"
         placeholder="Please enter the movie name in English"
+        onChange={handleChange}
+        disabled={isLoading}
       />
+      {errors.title && <p>{errors.title}</p>}
     </SearchBarForm>
   );
 };
