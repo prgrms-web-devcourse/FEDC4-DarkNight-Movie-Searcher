559cd966e2597f25d4180b5f644bcee635517191
kutta97 <yhjin04170@gmail.com>
2023-08-22 18:51:36 +0900

    refactor: SearchBar 컴포넌트에서 SearchBarButton 컴포넌트 분리

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,27 +1,9 @@
 import React from 'react';
 import PropType from 'prop-types';
-import styled from '@emotion/styled';
 import useForm from '../../hooks/useForm';
 import SearchBarForm from './SearchBarForm';
 import TextInput from '../Input/TextInput';
-import SearchIcon from '../../assets/icons/SearchIcon';
-
-const SearchBarButton = styled.button`
-  position: absolute;
-  right: 5px;
-  outline: none;
-  border: none;
-
-  display: block;
-  height: 46px;
-  width: 46px;
-  background-color: #888;
-  border-radius: 50%;
-
-  &:hover {
-    background-color: #aaaaaa;
-  }
-`;
+import SearchBarButton from './SearchBarButton';
 
 const SearchBar = ({ onSubmit }) => {
   const { errors, isLoading, handleChange, handleSubmit } = useForm({
@@ -44,9 +26,7 @@ const SearchBar = ({ onSubmit }) => {
         css={{ marginRight: 32 }}
       />
       {errors.title && <p>{errors.title}</p>}
-      <SearchBarButton type="submit">
-        <SearchIcon size={'23px'} color="#333" />
-      </SearchBarButton>
+      <SearchBarButton />
     </SearchBarForm>
   );
 };
