651ebb7e0bc7fb06c2ba88c6fb52987097f30b08
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-08-22 16:30:09 +0900

    [✨Feature]header는 고정시키는 방법으로 children기능 추가

--- src/routes/movieRouter.js
+++ src/routes/movieRouter.js
@@ -1,11 +1,22 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Error from '../components/Error';
 import Root from '../components/Root';
+import Movies from '../components/Movies';
 
 export const movieRouter = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <Error />,
+    children: [
+      {
+        path: '/movies',
+        element: <Movies />,
+      },
+      {
+        path: '/movies/:movieID',
+        element: <h3></h3>,
+      },
+    ],
   },
 ]);
