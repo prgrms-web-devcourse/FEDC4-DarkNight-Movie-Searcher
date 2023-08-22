6cc5e3f10367196bde925faba9cb98daa81bab1e
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-08-22 17:45:48 +0900

    [✨Fix]dummy data생성, 리스트 click시 detail페이지로 이동

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Movies() {
-  const params = 5; // 5번영화 선택 
+  const navigate = useNavigate();
+  const DUMMY_DATA = [
+    { id: 1, name: '영화1' },
+    { id: 2, name: '영화1' },
+    { id: 3, name: '영화1' },
+    { id: 4, name: '영화1' },
+    { id: 5, name: '영화1' },
+    { id: 6, name: '영화1' },
+  ];
+
+  const navigation = (movieID) => {
+    navigate(`/movies/${movieID}`);
+  };
   return (
     <div>
-      <Link to={`/movies/${params}`}>클릭</Link>
+      {DUMMY_DATA.map(({ id, name }) => (
+        <li
+          key={id}
+          onClick={() => {
+            navigation(id);
+          }}
+        >
+          <h2>영화 {name}</h2>
+        </li>
+      ))}
     </div>
-  )
+  );
 }
