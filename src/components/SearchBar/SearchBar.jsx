7ab901d654c900c5e3b9a8435c80f4f21d8ba852
kutta97 <yhjin04170@gmail.com>
2023-08-22 18:27:51 +0900

    refactor: SearchBar 컴포넌트에서 SearchBarForm 컴포넌트 분리

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
 import PropType from 'prop-types';
 import styled from '@emotion/styled';
-import TextInput from '../Input/TextInput';
 import useForm from '../../hooks/useForm';
+import SearchBarForm from './SearchBarForm';
+import TextInput from '../Input/TextInput';
 import SearchIcon from '../../assets/icons/SearchIcon';
-
-const SearchBarForm = styled.form`
-  position: relative;
-  display: flex;
-  align-items: center;
-  width: 100%;
-  padding: 5px 20px;
-  font-size: 26px;
-  color: #ffd26b;
-  border-radius: 30px;
-  background-color: #222;
-
-  &:focus-within {
-    border: 1px solid #333;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-  }
-`;
 
 const SearchBarButton = styled.button`
   position: absolute;
