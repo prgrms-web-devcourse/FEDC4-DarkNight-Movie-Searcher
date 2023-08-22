d75cbc25782f393e417b10bb929fd417e27135b5
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-08-22 16:16:23 +0900

    [✨Add]가독성을 위한 form생성 -> 모든 라우팅 내 고정됨

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -4,8 +4,14 @@ import { Outlet } from 'react-router-dom'
 export default function Root() {
   return (
     <div>
-      <h1>가장 최상위 root 컴포넌트</h1>
-
+        {/* 고정 */}
+      <header>
+        <form>
+          <input type="text" />
+          <button>버튼</button>
+        </form>
+      </header>
+      {/* 고정 */}
       <Outlet />
     </div>
   )
