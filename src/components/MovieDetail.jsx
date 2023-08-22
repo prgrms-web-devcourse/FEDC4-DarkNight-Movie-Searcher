74ca5d45e456a18873249cbfd325bc024861718a
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-08-22 18:03:54 +0900

    [✨Feat]detail기능 확인을 위한 API호출기능 구현

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function MovieDetail() {
   const { movieID } = useParams();
-  return <div>선택한 영화 번호 - {movieID}</div>;
+  const [selectedMovie, setSelectedMove] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://omdbapi.com?apikey=7035c60c&i=${movieID}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setSelectedMove(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h2>선택한 영화 번호 - {movieID}</h2>
+      {selectedMovie &&
+        selectedMovie.map(({ Title, Year, imdbID, Poster }) => (
+          <li key={imdbID}>
+            <img src={Poster} alt="" />
+            <h2>{Title}</h2>
+            <p>{Year}</p>
+          </li>
+        ))}
+    </div>
+  );
 }
+
+// url에 movieID파라미터로 넣고 fetch요청 하시면 됩니다!!!
+// useEffect => []마운트 할 때로 놓고 요청하시면 됩니다!
