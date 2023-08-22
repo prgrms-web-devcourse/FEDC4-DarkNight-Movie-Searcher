ffcc6a2fd7fc7ee3d92387e349ab1f79e19febb6
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-08-22 16:11:32 +0900

    [✨Add]header에 searchbar를 고정하기 위한 outlet기능 추가

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function Root() {
   return (
     <div>
       <h1>가장 최상위 root 컴포넌트</h1>
+
+      <Outlet />
     </div>
   )
 }
