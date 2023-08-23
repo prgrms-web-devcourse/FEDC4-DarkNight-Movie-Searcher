f0b8516e4acfc22e3802049206947af3e91101e2
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-08-23 17:20:33 +0900

    add : Loading css추가

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,46 +1,53 @@
-// import React from 'react';
+import React from 'react';
+import { css } from '@emotion/react';
+const style = css`
+  & {
+    width: 100vw;
+    height: 100vh;
+  }
+  & > span {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background-color: gray;
+    border-radius: 50%;
+    animation: loading 1s 0s linear infinite;
+  }
 
-// // const LoadingAni = styled.div`
-// //   & {
-// //     width: 100vw;
-// //     height: 100vh;
-// //   }
-// //   & > span {
-// //     display: inline-block;
-// //     width: 10px;
-// //     height: 10px;
-// //     background-color: gray;
-// //     border-radius: 50%;
-// //     animation: loading 1s 0s linear infinite;
-// //   }
+  & > span:nth-child(1) {
+    animation-delay: 0s;
+    background-color: red;
+  }
 
-// //   & > span:nth-child(1) {
-// //     animation-delay: 0s;
-// //     background-color: red;
-// //   }
+  & > span:nth-child(2) {
+    animation-delay: 0.2s;
+    background-color: orange;
+  }
 
-// //   & > span:nth-child(2) {
-// //     animation-delay: 0.2s;
-// //     background-color: orange;
-// //   }
+  & > span:nth-child(3) {
+    animation-delay: 0.4s;
+    background-color: yellowgreen;
+  }
 
-// //   & > span:nth-child(3) {
-// //     animation-delay: 0.4s;
-// //     background-color: yellowgreen;
-// //   }
+  @keyframes loading {
+    0%,
+    100% {
+      opacity: 0;
+      transform: scale(0.5);
+    }
+    50% {
+      opacity: 1;
+      transform: scale(1.2);
+    }
+  }
+`;
 
-// //   @keyframes loading {
-// //     0%,
-// //     100% {
-// //       opacity: 0;
-// //       transform: scale(0.5);
-// //     }
-// //     50% {
-// //       opacity: 1;
-// //       transform: scale(1.2);
-// //     }
-// //   }
-// // `;
-// export default function Loading() {
-//   return ()
-// }
+export default function Loading() {
+  return (
+    <div style={style}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
