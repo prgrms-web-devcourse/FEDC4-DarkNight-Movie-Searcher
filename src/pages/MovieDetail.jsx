4a1cd24aa0eea2ae03be90d339c7d90cceeaf022
nocheol <qkdl60@gmail.com>
2023-08-23 14:46:39 +0900

    feat: MoviDetail 페이지 추가

---
+++ src/pages/MovieDetail.jsx
@@ -0,0 +1,64 @@
+import React, { useContext, useEffect, useMemo, useState } from 'react';
+import { movieContext } from '../App';
+import styled from '@emotion/styled';
+import MovieInformation from '../components/MovieInformation/MovieInformation';
+import { palette } from '../assets/stylesConstants';
+
+const Page = styled.div`
+  margin: 0 auto;
+  padding-bottom: 100px;
+  background-color: ${palette.backgroundColor};
+`;
+
+const BackgroundPoster = styled.div`
+  background-image: url(${(props) => props.poster});
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: calc(100vh - 200px);
+`;
+
+const InformationWrapper = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  gap: 35px;
+`;
+
+const MovieDetail = () => {
+  const movieDetail = useContext(movieContext);
+  const [positionY, setPositionY] = useState(0);
+
+  const NOT_AVILABLE_POSTER =
+    'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX900.jpg';
+
+  const onScroll = () => {
+    setPositionY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
+  const checkedPoster = useMemo(() => {
+    if (!movieDetail.Poster) return '';
+    if (movieDetail.poster === 'N/A') return NOT_AVILABLE_POSTER;
+    return movieDetail.Poster.replace('SX300', 'SX900');
+  }, [movieDetail]);
+
+  return (
+    <Page>
+      <BackgroundPoster
+        poster={movieDetail.Poster && checkedPoster}
+        style={{ backgroundPositionY: positionY * -1 }}
+      />
+      <InformationWrapper>
+        <MovieInformation />
+      </InformationWrapper>
+    </Page>
+  );
+};
+export default MovieDetail;
