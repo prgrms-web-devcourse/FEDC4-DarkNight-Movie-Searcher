016ce143856ac65f5d7d56ad78f551aeed51e2b2
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-08-23 16:07:53 +0900

    chore : styled-components 패키지 제거

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,47 +1,46 @@
-import React from 'react';
-import styled from 'styled-components';
+// import React from 'react';
 
-const LoadingAni = styled.div`
-  & {
-    width: 100vw;
-    height: 100vh;
-  }
-  & > span {
-    display: inline-block;
-    width: 10px;
-    height: 10px;
-    background-color: gray;
-    border-radius: 50%;
-    animation: loading 1s 0s linear infinite;
-  }
+// // const LoadingAni = styled.div`
+// //   & {
+// //     width: 100vw;
+// //     height: 100vh;
+// //   }
+// //   & > span {
+// //     display: inline-block;
+// //     width: 10px;
+// //     height: 10px;
+// //     background-color: gray;
+// //     border-radius: 50%;
+// //     animation: loading 1s 0s linear infinite;
+// //   }
 
-  & > span:nth-child(1) {
-    animation-delay: 0s;
-    background-color: red;
-  }
+// //   & > span:nth-child(1) {
+// //     animation-delay: 0s;
+// //     background-color: red;
+// //   }
 
-  & > span:nth-child(2) {
-    animation-delay: 0.2s;
-    background-color: orange;
-  }
+// //   & > span:nth-child(2) {
+// //     animation-delay: 0.2s;
+// //     background-color: orange;
+// //   }
 
-  & > span:nth-child(3) {
-    animation-delay: 0.4s;
-    background-color: yellowgreen;
-  }
+// //   & > span:nth-child(3) {
+// //     animation-delay: 0.4s;
+// //     background-color: yellowgreen;
+// //   }
 
-  @keyframes loading {
-    0%,
-    100% {
-      opacity: 0;
-      transform: scale(0.5);
-    }
-    50% {
-      opacity: 1;
-      transform: scale(1.2);
-    }
-  }
-`;
-export default function Loading() {
-  return <LoadingAni />;
-}
+// //   @keyframes loading {
+// //     0%,
+// //     100% {
+// //       opacity: 0;
+// //       transform: scale(0.5);
+// //     }
+// //     50% {
+// //       opacity: 1;
+// //       transform: scale(1.2);
+// //     }
+// //   }
+// // `;
+// export default function Loading() {
+//   return ()
+// }
