2d4dae4e6e86d8b35f50286b44cc894f22f9b901
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-08-23 17:37:04 +0900

    fix : 불필요한 옵션 제거

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
     browser: true,
     node: true,
     es2021: true,
-    node: true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
   overrides: [],
@@ -13,7 +12,6 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
-    'react/react-in-jsx-scope': 'off',
     'react/no-unknown-property': ['error', { ignore: ['css'] }],
   },
 };
