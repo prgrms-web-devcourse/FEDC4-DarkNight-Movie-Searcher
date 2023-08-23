e56929d95632d10e8c5e49cd6d3a9a13df65c1ce
Seunghee Lee <49032882+eeseung@users.noreply.github.com>
2023-08-23 16:45:19 +0900

    Feature/context api (#4)
    * feat: MovieProvider 구현
    
    * feat: App에 MovieProvider 연결

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Global } from '@emotion/react';
 import './App.css';
 import { globalStyles } from './assets/globalStyles';
+import MovieProvider from './contexts/MovieProvider';
 
 function App() {
   return (
-    <div className="App">
-      <Global styles={globalStyles} />
-    </div>
+    <MovieProvider>
+      <div className="App">
+        <Global styles={globalStyles} />
+      </div>
+    </MovieProvider>
   );
 }
 
